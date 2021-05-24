ce8de2af7b2ca647e6d4111262bde332ad586b66
EdgarDorausch <login@doraus.ch>
2021-05-24 21:35:01 +0200

    Added position loader

--- read_data.py
+++ read_data.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import re
 from typing import *
 if TYPE_CHECKING:
     pass
@@ -31,3 +32,39 @@ def load_signal_data(dir_path: str, file_indices: List[int]) -> np.ndarray:
         print(f'Loaded: {progress:3.1f}%', end='\r')
 
     return array
+
+
+
+
+def match_position(line: str) -> Tuple[np.ndarray, np.ndarray]:
+    FLOAT_GROUP: Callable[[str], str] = lambda name: f'(?P<{name}>-?\d+\.\d+)'
+
+    PATTERN = r'Piezo\[' + \
+        FLOAT_GROUP("Px") + ' ' + \
+        FLOAT_GROUP("Py") + ' ' + \
+        FLOAT_GROUP("Pz") + \
+    r'\]\tcMUT\[ ' + \
+        FLOAT_GROUP("Cx") + ' ' + \
+        FLOAT_GROUP("Cy") + '  ' + \
+        FLOAT_GROUP("Cz") + \
+    r'\]'
+
+    pattern = re.compile(PATTERN)
+    match = pattern.match(line)
+
+    if match is None:
+        raise Exception(f'Could not match expression "{line}"')
+     
+    p_array = np.array([float(match.group('Px')), float(match.group('Py')), float(match.group('Pz'))])
+    c_array = np.array([float(match.group('Cx')), float(match.group('Cy')), float(match.group('Cz'))])
+    return (p_array, c_array)
+
+def load_position_data(dir_path: str):
+    file_path = path.join(dir_path, '_transducer_positions.txt')
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        return [match_position(line) for line in lines]
+
+
+if __name__ == '__main__':
+    print(load_position_data('./data/toastgitter/')[:4])
